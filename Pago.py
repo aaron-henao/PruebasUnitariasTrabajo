336abfbc0b558efcaa7c051d064d687cfc597236
aaron-henao <110606543+aaron-henao@users.noreply.github.com>
2023-08-18 07:26:02 -0500

    Update Pago.py
    Creación de una consola

--- Pago.py
+++ Pago.py
@@ -68,35 +68,40 @@ class DeudaTarjetaCredito:
             plan_con_abono.append((cuota, interes, principal, saldo))
 
         return plan_con_class DeudaTarjetaCredito:
-    # ... (el código de la clase que proporcionaste)
 
-def main():
-    try:
-        monto_compra = float(input("Ingrese el monto de la compra: "))
-        tasa_interes = float(input("Ingrese la tasa de interés anual (%): "))
-        num_cuotas = int(input("Ingrese el número de cuotas: "))
-        
-        deuda = DeudaTarjetaCredito(monto_compra, tasa_interes, num_cuotas)
-        
-        print("Cuota mensual:", deuda.cuota_mensual)
-        print("Intereses totales a pagar:", deuda.calcular_intereses_totales())
-        
-        plan_amortizacion = deuda.calcular_plan_amortizacion()
-        print("\nPlan de amortización:")
-        for cuota, interes, principal, saldo in plan_amortizacion:
-            print(f"Cuota: {cuota:.2f} | Interés: {interes:.2f} | Principal: {principal:.2f} | Saldo restante: {saldo:.2f}")
-        
-        num_cuota_abono = int(input("\nIngrese el número de cuota para abonar extra: "))
-        monto_abono = float(input("Ingrese el monto a abonar extra: "))
-        plan_con_abono = deuda.calcular_efecto_abono_extra(monto_abono, num_cuota_abono)
-        
-        print("\nPlan de amortización con abono extra:")
-        for cuota, interes, principal, saldo in plan_con_abono:
-            print(f"Cuota: {cuota:.2f} | Interés: {interes:.2f} | Principal: {principal:.2f} | Saldo restante: {saldo:.2f}")
-    except ValueError as e:
-        print("Error:", e)
+PagoConsole
+from Pago import DeudaTarjetaCredito
+"""
+La interfaz de usuario del programa debe separarse del modulo 
+que contiene la lógica.
 
-if __name__ == "__main__":
-    main()
+En este caso, la interfaz de usuario queda en CreditCardConsole.py
+y la lógica queda en Payment.py
+"""
+
+print("Este programa le permite calcular la cuota a pagar por una compra con tarjeta de credito")
+monto_compra = float( input("Monto de la compra:") )
+tasa_interes = float( input("Tasa de interés de la tarjeta:") )
+num_cuotas = int( input("Numero de cuotas en que va a diferir la compra:") )
+
+deuda = DeudaTarjetaCredito(monto_compra, tasa_interes, num_cuotas)
+
+print("Cuota mensual:", deuda.cuota_mensual)
+print("Intereses totales a pagar:", deuda.calcular_intereses_totales())
+
+plan_amortizacion = deuda.calcular_plan_amortizacion()
+print("\nPlan de amortización:")
+for cuota, interes, principal, saldo in plan_amortizacion:
+    print(f"Cuota: {cuota:.2f} | Interés: {interes:.2f} | Principal: {principal:.2f} | Saldo restante: {saldo:.2f}")
+
+num_cuota_abono = int(input("\nIngrese el número de cuota para abonar extra: "))
+monto_abono = float(input("Ingrese el monto a abonar extra: "))
+plan_con_abono = deuda.calcular_efecto_abono_extra(monto_abono, num_cuota_abono)
+
+"""print("\nPlan de amortización con abono extra:")
+for cuota, interes, principal, saldo in plan_con_abono:
+    print(f"Cuota: {cuota:.2f} | Interés: {interes:.2f} | Principal: {principal:.2f} | Saldo restante: {saldo:.2f}")
+except ValueError as e:
+print("Error:", e)"""
 
 
